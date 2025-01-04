def33f0ab9dc79930e463da7af2bfd1120017ca9
Colin Woodbury <colin@fosskers.ca>
2025-01-04 18:18:56 +0900

    [build] add an ECL-specific build script

---
+++ build.lisp
@@ -0,0 +1,27 @@
+(require :asdf)
+
+;; Force ASDF to only look here for systems.
+(asdf:initialize-source-registry `(:source-registry (:tree ,(uiop:getcwd)) :ignore-inherited-configuration))
+
+(format t "--- LOADING SYSTEM ---~%")
+(declaim (optimize (speed 3) (debug 1) (safety 1)))
+(asdf:load-system :vend)
+
+#-ecl
+(progn
+  (format t "--- NO OP! ---~%")
+  (format t "Use ECL instead.~%")
+  #+sbcl (sb-ext:exit :code 1))
+
+#+ecl
+(progn
+  (format t "--- COMPILING EXECUTABLE ---~%")
+  (asdf:make-build :vend
+                   :type :program
+                   :move-here #p"./"
+                   :epilogue-code
+                   '(progn
+                     (vend:main)
+                     (si:exit)))
+  (format t "--- DONE ---~%")
+  (si:exit))
--- src/vend.lisp
+++ src/vend.lisp
@@ -1,3 +1,23 @@
 (defpackage vend
   (:use :cl)
+  (:local-nicknames (#:p #:filepaths))
+  (:export #:main)
   (:documentation "Simply vendor your Common Lisp project dependencies."))
+
+(in-package :vend)
+
+(defun asd-files (dir)
+  "Yield the pathnames of all `.asd' files found in the given DIR."
+  (directory (p:join dir "*.asd")))
+
+#++
+(asd-files "./")
+
+#++
+(defun sexps-from-file (path)
+  "Read the sexps from a given file PATH without evaluating them."
+  (let ((*read-eval* nil))
+    ()))
+
+(defun main ()
+  (format t "Done.~%"))
